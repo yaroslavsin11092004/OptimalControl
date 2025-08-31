926eda70169c5a6ae8405c39e0412551a3731865
yaroslavsin11092004 <yaros.sinyakov@mail.ru>
2025-08-31 18:46:10 +0300

    finish TasksApi

--- CoreAlgoritm/source/TasksApi.cpp
+++ CoreAlgoritm/source/TasksApi.cpp
@@ -157,3 +157,114 @@ std::shared_ptr<std::vector<Equation>> TasksApi::generate_linked(int dim)
 		throw std::runtime_error(e.what());
 	}
 }
+net::awaitable<int> TasksApi::call_dimension()
+{
+	try 
+	{
+		auto resolver = tcp::resolver(co_await net::this_coro::executor);
+		auto socket = tcp::socket(co_await net::this_coro::executor);
+		auto endpoint = co_await resolver.async_resolve(this->host, this->port, net::use_awaitable);
+		co_await net::async_connect(socket, endpoint, net::use_awaitable);
+		Request req{http::verb::post, "/dimension",11};
+		req.set(http::field::content_type, "application/json");
+		req.prepare_payload();
+		co_await http::async_write(socket, req, net::use_awaitable);
+		http::response<http::string_body> resp;
+		beast::flat_buffer buffer;
+		co_await http::async_read(socket, buffer, resp, net::use_awaitable);
+		json resp_json = json::parse(resp.body());
+		co_return resp_json["result"].get<int>();
+	}
+	catch(const std::exception& e)
+	{
+		throw std::runtime_error(e.what());
+	}
+}
+net::awaitable<double> TasksApi::call_param_s(matrix<double>& x, matrix<double>& u, matrix<double>& optim_u)
+{
+	try 
+	{
+		json req_json;
+		req_json["x"] = *x.store_handle();
+		req_json["x_size_row"] = x.size_row();
+		req_json["x_size_col"] = x.size_col();
+		req_json["u"] = *u.store_handle();
+		req_json["u_size_row"] = u.size_row();
+		req_json["u_size_col"] = u.size_col();
+		req_json["optim_u"] = *optim_u.store_handle();
+		req_json["optim_u_size_row"] = optim_u.size_row();
+		req_json["optim_u_size_col"] = optim_u.size_col();
+		auto resolver = tcp::resolver(co_await net::this_coro::executor);
+		auto socket = tcp::socket(co_await net::this_coro::executor);
+		auto endpoint = co_await resolver.async_resolve(this->host, this->port, net::use_awaitable);
+		co_await net::async_connect(socket, endpoint, net::use_awaitable);
+		Request req{http::verb::post, "/param_s", 11};
+		req.set(http::field::content_type, "application/json");
+		req.body() = req_json.dump();
+		req.prepare_payload();
+		co_await http::async_write(socket, req, net::use_awaitable);
+		http::response<http::string_body> resp;
+		beast::flat_buffer buffer;
+		co_await http::async_read(socket, buffer, resp, net::use_awaitable);
+		auto resp_json = json::parse(resp.body());
+		co_return resp_json["result"].get<double>();
+	}
+	catch(const std::exception& e)
+	{
+		throw std::runtime_error(e.what());
+	}
+}
+int TasksApi::dimension()
+{
+	try 
+	{
+		std::promise<int> result_promise;
+		auto result_future = result_promise.get_future();
+		net::co_spawn(*this->ioc,
+		[self = shared_from_this(), result_promise = std::move(result_promise)] mutable -> net::awaitable<void>
+		{
+			try 
+			{
+				int result = co_await self->call_dimension();
+				result_promise.set_value(result);
+			}
+			catch(...)
+			{
+				result_promise.set_exception(std::current_exception());
+			}
+		},
+		net::detached);
+		return result_future.get();
+	}
+	catch(const std::exception& e)
+	{
+		throw std::runtime_error(e.what());
+	}
+}
+double TasksApi::param_s(matrix<double>& x, matrix<double>& u, matrix<double>& optim_u)
+{
+	try 
+	{
+		std::promise<double> result_promise;
+		auto result_future = result_promise.get_future();
+		net::co_spawn(*this->ioc,
+		[self = shared_from_this(), x,u,optim_u, result_promise = std::move(result_promise)]()mutable -> net::awaitable<void>
+		{
+			try 
+			{
+				double result = co_await self->call_param_s(x,u,optim_u);
+				result_promise.set_value(result);
+			}
+			catch(...)
+			{
+				result_promise.set_exception(std::current_exception());
+			}
+		},
+		net::detached);
+		return result_future.get();
+	}
+	catch(const std::exception& e)
+	{
+		throw std::runtime_error(e.what());
+	}
+}
